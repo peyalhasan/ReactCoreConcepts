65d3ccac69707ea987cf7b5c7dd00ee6e2a5659a
Peyal Hasan <peyalhasan143@gmail.com>
2025-10-26 22:45:37 +0600

    Rendering

--- src/Components/Team.jsx
+++ src/Components/Team.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react"
+
 export default function Team(){
+    const [team, setTeam] = useState(11)
+    const handleAdd = () =>{
+        setTeam(t => t + 1)
+    }
+    const handleReduce = () =>{
+        setTeam(t => t -1)
+    }
     return (
-        <div>
-            <h3>Players: </h3>
+        <div className=" border-2 border-purple-600 m-4 p-4 rounded-2xl  ">
+            <h3>Players: {team}</h3>
+            <div className="flex gap-2 "> 
+            <button onClick={handleAdd}>Add</button>
+            <button onClick={handleReduce}>Reduce</button>
+            </div>
         </div>
     )
 }
